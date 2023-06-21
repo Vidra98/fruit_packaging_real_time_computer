d6b302783d479dacf9470c36954bca28be3df6d0
Your Name <you@example.com>
2023-06-21 16:28:09 +0200

    some changes : new demo, added grid to architecture and thing like that

--- src/utils/ROS_utils.py
+++ src/utils/ROS_utils.py
@@ -96,10 +96,15 @@ class gridRegistrator():
         self.acq_pos = None
 
     def callback(self, poseArray, disposability_grid_msg):
+        if abs(np.sum(self.rbt.dq)) > 0.005:
+            return
         self.poses = poseArray.poses
         self.disposability_grid = self.bridge.imgmsg_to_cv2(disposability_grid_msg, "mono8")
         self.registration_time = time.time()
         self.acq_pos = get_camera_pose(self.rbt, ee_depth=-0.1150)
+
+    def get_acq_pos(self):
+        return self.acq_pos
 
     def get_poses(self):
         return self.poses
@@ -109,6 +114,11 @@ class gridRegistrator():
     
     def get_registration_time(self):
         return self.registration_time
+    
+    def clear_grid(self):
+        self.disposability_grid = np.ones_like(self.disposability_grid) * 255
+        self.disposability_grid[0,0] = 0
+        self.disposability_grid[-1, -1] = 0
 
     def set_cell_occupancy(self, cell_idx, occupancy):
         if self.disposability_grid is None:
